3354a5f2e30859cad936a60e7e2aeb494ffc2754
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-13 07:34:22 +0800

    shell-format.py:fix path

--- tools/shell-format.py
+++ tools/shell-format.py
@@ -5,12 +5,12 @@ def check_script(script: str) -> str:
     Check the shell script using shellcheck and shfmt.
     """
     # Check using shfmt
-    shfmt = subprocess.run(["shfmt", "-d", src/rootfstool/rootfstool src/container], capture_output=True)
+    shfmt = subprocess.run(["shfmt", "-d", src/rootfstool/rootfstool src/container/container], capture_output=True)
     if shfmt.returncode != 0:
         return "Step 1 Failed."
 
     # Check using shellcheck
-    shellcheck = subprocess.run(["shellcheck", src/rootfstool/rootfstool src/container], capture_output=True)
+    shellcheck = subprocess.run(["shellcheck", src/rootfstool/rootfstool src/container/container], capture_output=True)
     if shellcheck.returncode != 0:
         return "Step 2 Failed."
 
