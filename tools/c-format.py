5acbe0c6ef36726a7d78cbd8e60b037cc3b0312c
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-13 07:14:56 +0800

    c-format.py:use clang-tidy

--- tools/c-format.py
+++ tools/c-format.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def check_c_files():
-    cmd = "find . -name \"*.c\" -o -name \"*.h\" | xargs clang-format -style=file"
+    cmd = "find . -name \"*.c\" -o -name \"*.h\" | xargs clang-tidy --checks=-clang-analyzer-security.insecureAPI.strcpy,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling  -fix"
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if result.returncode != 0:
         print("check failed")
