fb45f5201d7f427c066f7ebce9242bb6b7e93836
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-13 07:04:19 +0800

    Update c-format.py

--- tools/c-format.py
+++ tools/c-format.py
@@ -1,15 +1,11 @@
 import os
 import subprocess
 
-def check_c_files(path):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(".c"):
-                file_path = os.path.join(root, file)
-                cmd = f"clang-format -style=file {file_path}"
-                result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                if result.returncode != 0:
-                    print(f"check failed: {file_path}")
-                    print(result.stderr.decode())
+def check_c_files():
+    cmd = "find . -name \"*.c\" -o -name \"*.h\" | xargs clang-format -style=file"
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print("check failed")
+        print(result.stderr.decode())
                     
-check_c_files(".")
+check_c_files()
