9b35aa426dfe2c75ecdd063a11d4ead2bae4e3b2
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-23 23:50:26 +0800

    Create build_config.mk

---
+++ debian/build_config.mk
@@ -0,0 +1,17 @@
+#!/usr/bin/make -f
+
+export CC = /bin/clang
+export LD = /bin/ld.lld
+export DH_VERBOSE = 1
+
+CC_BIN = $(test -f /usr/bin/clang)
+  
+DEB_BUILD_ARCH ?= $(shell dpkg-architecture -qDEB_BUILD_ARCH)
+DEB_HOST_MULTIARCH ?= $(shell dpkg-architecture -qDEB_HOST_MULTIARCH)
+
+all:
+indef CC_BIN
+        @echo
+else
+        echo  Clang not Found ! && exit 127
+endif
