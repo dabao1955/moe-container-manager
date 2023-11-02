50d4aee2fa4c41d72aacc1a72d065073f26c4804
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-11-02 20:46:06 +0800

    6

---
+++ src/pkc/build/CMakeFiles/pkc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pkc.dir/pkc.c.o"
+  "CMakeFiles/pkc.dir/pkc.c.o.d"
+  "pkc"
+  "pkc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pkc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
