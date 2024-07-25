b5b3b66997a91c00e7536c271a895b8e5e075fc9
dabao1955 <dabao1955@163.com>
2024-07-25 17:08:17 +0800

    fix

--- src/interface/interface.cmake
+++ src/interface/interface.cmake
@@ -18,7 +18,7 @@ add_compile_options("-D_FORTIFY_SOURCE=3 -enable-trivial-auto-var-init-zero-know
 # add the executable
 file(GLOB SOURCES ${CMAKE_SOURCE_DIR}/interface/src/*.cpp)
 
-include_directories(${CMAKE_SOURCE_DIR}/include)
+include_directories(${CMAKE_SOURCE_DIR}/interface/include)
 
 # add the executable
 add_executable(interface ${SOURCES})
