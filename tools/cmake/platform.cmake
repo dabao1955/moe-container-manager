a089f63de83b801ef4065fa1953208c20a4c189b
dabao1955 <dabao1955@163.com>
2024-08-01 11:20:35 +0800

    Add CXX flags check
    Signed-off-by: dabao1955 <dabao1955@163.com>

--- tools/cmake/platform.cmake
+++ tools/cmake/platform.cmake
@@ -1,3 +1,3 @@
-if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
+if (NOT "${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
    message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
 endif ()
--- tools/cmake/setting.cmake
+++ tools/cmake/setting.cmake
@@ -2,3 +2,27 @@ if(${CMAKE_VERSION} VERSION_GREATER "3.14.99" AND NOT DEFINED CMAKE_MSVC_RUNTIME
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded")
     cmake_policy(SET CMP0091 NEW)
 endif()
+
+include(CheckCXXCompilerFlag)
+set(FLAGS_TO_CHECK
+    "-enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
+    "-fdata-sections"
+    "-ffunction-sections"
+    "-fstack-clash-protection"
+    "-fstack-protector-all"
+    )
+
+set(UNSUPPORTED_FLAGS "")
+
+foreach(FLAG ${FLAGS_TO_CHECK})
+    string(REPLACE "-" "_" FLAG_VAR "CXXFLAG_CHECK_${FLAG}")
+    check_cxx_compiler_flag(${FLAG} ${FLAG_VAR})
+    if(NOT ${FLAG_VAR})
+        list(APPEND UNSUPPORTED_FLAGS ${FLAG})
+    endif()
+endforeach()
+
+if(UNSUPPORTED_FLAGS)
+    message(FATAL_ERROR "ERROR: The following CXX flags are not supported: ${UNSUPPORTED_FLAGS}")
+endif()
+
