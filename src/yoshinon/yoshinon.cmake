5f18e90e2ea4ece7567325961399429fd6dd11cf
dabao1955 <dabao1955@163.com>
2024-08-01 16:37:16 +0800

    Add go compiler info
    Signed-off-by: dabao1955 <dabao1955@163.com>

--- src/yoshinon/yoshinon.cmake
+++ src/yoshinon/yoshinon.cmake
@@ -5,9 +5,23 @@ project(yoshinon LANGUAGES NONE)
 
 find_program(GO_COMPILER go)
 
-if(NOT GO_COMPILER)
+if(GO_COMPILER)
+    execute_process(
+        COMMAND ${GO_COMPILER} version
+        OUTPUT_VARIABLE GO_VERSION_OUTPUT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    string(REGEX MATCH "go version go([0-9]+\\.[0-9]+\\.[0-9]+)" GO_VERSION_MATCH ${GO_VERSION_OUTPUT})
+    if (GO_VERSION_MATCH)
+        set(GO_VERSION ${CMAKE_MATCH_1})
+    else()
+        set(GO_VERSION "unknown")
+    endif()
+    message(STATUS "Found Golang Compiler: ${GO_COMPILER} (found version ${GO_VERSION})")
+else ()
     message(FATAL_ERROR "Go compiler not found")
-endif()
+endif ()
+
 
 # Capture the latest commit ID from the git log
 execute_process(
