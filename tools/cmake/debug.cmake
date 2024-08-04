c4b093a7af6624ecd09d9cd19567eeec9608d3e9
dabao1955 <dabao1955@163.com>
2024-08-04 18:51:56 +0800

    Add more warning check while compiling
    Signed-off-by: dabao1955 <dabao1955@163.com>

--- tools/cmake/debug.cmake
+++ tools/cmake/debug.cmake
@@ -3,10 +3,17 @@ option(DEBUG_MODE "Enable debugging" ON)
 if (DEBUG_MODE)
     message(WARNING "Warning: debug mode is enabled.")
     set(CMAKE_BUILD_TYPE Debug)
-    add_compile_options("-Wpedantic")
-    add_compile_options("-pedantic")
-    add_compile_options("-Wextra")
+    add_compile_options(-g -O0)
+    add_compile_options(
+        -Wpedantic -pedantic
+        -Wextra
+        -Wredundant-decls -Wlogical-op
+        -Wstrict-overflow=5 -Winit-self
+        -Wuninitialized -Wsign-conversion
+        -Wcast-qual
+    )
 else()
     set(CMAKE_BUILD_TYPE Release)
-    add_compile_options("-Oz")
+    add_compile_options(-Oz)
+    add_compile_options(-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_REENTRANT)
 endif()
