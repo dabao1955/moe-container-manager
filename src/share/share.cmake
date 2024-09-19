d9700ab8ab00fe391572b2c7d9c3c7f89e328b57
dabao1955 <dabao1955@163.com>
2024-09-20 00:00:41 +0800

    COMMENT test

--- src/share/share.cmake
+++ src/share/share.cmake
@@ -6,12 +6,14 @@ project(share LANGUAGES NONE)
 add_custom_target(
     copyright ALL
     COMMAND cp ../LICENSE ${CMAKE_SOURCE_DIR}/../out/share/doc/moe-container-manager/
+    COMMENT "Copying LICENSE"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} # Set correct path where main.go is located
 )
 
 add_custom_target(
     share ALL
     COMMAND cp share/*.sh ${CMAKE_SOURCE_DIR}/../out/share/moe-container-manager/
+    COMMENT "Copying necessary shell scripts"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} # Set correct path where main.go is located
     DEPENDS copyright
 )
@@ -19,6 +21,7 @@ add_custom_target(
 add_custom_target(
     proc ALL
     COMMAND tar -xf share/proc.tar.xz -C ${CMAKE_SOURCE_DIR}/../out/share/moe-container-manager/proc/
+    COMMENT "Exacting proc files"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} # Set correct path where main.go is located
     DEPENDS share
 )
