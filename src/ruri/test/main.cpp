5cd43d55e43ab69dedaa0e74170dbcfd96b4a7c6
dabao1955 <dabao1955@163.com>
2024-11-13 20:19:47 +0800

    ci

--- src/ruri/test/main.cpp
+++ src/ruri/test/main.cpp
@@ -2,27 +2,30 @@
 #include <memory>
 #include <array>
 #include <cstdio>
-#include <cstdlib>
 
 int main() {
     std::array<char, 128> buffer;
+    std::string result;
 
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("sudo bash test-root.sh", "r"), pclose);
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("bash test-root.sh", "r"), pclose);
     if (!pipe) {
         std::cerr << "Failed to run script." << std::endl;
         return 1;
     }
 
+    int status = 0;
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        std::cout << buffer.data();
-        std::cout.flush();
+        result += buffer.data();
     }
 
-    int returnCode = pclose(pipe.release());
-    if (!WIFEXITED(returnCode) || WEXITSTATUS(returnCode) != 0) {
+    status = pclose(pipe.get());
+
+    if (status != 0) {
+        std::cerr << "Script failed with exit status: " << status << std::endl;
         return 1;
     }
 
+    std::cout << result;
     return 0;
 }
 
